1c14b65782de0ae657ed4b8e181fd684c089f32d
shkawan <shkawan@microsoft.com>
2016-09-21 01:43:12 +0900

    modify

--- lib/My_Utils.pm
+++ lib/My_Utils.pm
@@ -5,15 +5,20 @@ package My_Utils 0.01 {
   use Sys::Syslog qw(:DEFAULT setlogsock);
   use File::Basename;
   use LWP::UserAgent;
+  use JSON;
   use Exporter q(import);
 
   our @EXPORT_OK = qw( post_to_myslack logging );
 
-  our $SLACK_BASE_URL = qq{http://uname.link/slack/};
+  our $SLACK_API = $ENV{MY_SLACK_API};
   
   my $lwp;
   sub post_to_myslack {
-    my ($channel, $message) = @_;
+    my ($channel, $text) = @_;
+
+    if (not defined $SLACK_API) {
+      return;
+    }
 
     my $lwp //= do {
                      my $lwp = LWP::UserAgent->new;
@@ -21,8 +26,9 @@ package My_Utils 0.01 {
                      $lwp;
                    };
 
-    my $slack_url = $SLACK_BASE_URL . $channel;
-    $lwp->post( $slack_url, { message => $message } );
+    my $json = encode_json {"channel" => $channel, "username" => "webhookbot", "text" => $text, "icon_emoji"=>":ghost"};
+    my $response = $lwp->post( $SLACK_API, { payload => $json } );
+    return $response->code;
   }
 
   sub logging {
