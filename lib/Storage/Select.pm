28ad209fc62323bebfd9f0fa419b41c1065d1fc5
shkawan <shkawan@microsoft.com>
2016-10-02 22:37:22 +0900

    modify codes to make Super class

--- lib/Storage/Select.pm
+++ lib/Storage/Select.pm
@@ -1,29 +1,17 @@
 use strict;
 use warnings;
 package Storage::Select 0.01 {
-  use File::Basename;
-  use JSON;
-  use Data::Dumper;
-  require Storage;
-  require Storage::Config;
+  use Carp;
+  use Storage::Config;
 
-  my $config = Storage::Config->get;
-  my $dispatch;
+  sub new {
+    my $obj = bless {}, shift;
+    $obj->{config} = Storage::Config->get;
+    return $obj;
+  }
 
   sub get {
-    my ($name) = @_;
-    my $basename = basename $name;
-    my $dispatch_config = $config->{copy_module_params}->{dispatch_by_filename};
-    my $storage_name;
-    _DISPATCH_CONFIG_:
-    for my $c ( @$dispatch_config ) {
-      if ($basename =~ /^$c->{pattern}/i) {
-        $storage_name = $c->{storage};
-        last _DISPATCH_CONFIG_;
-      }
-    }
-    $storage_name //= $dispatch_config->[0]->{storage};
-    return Storage->new( $config->{storage}->{$storage_name} );
+    croak;
   }
 
 }
