a8031884bce284c9d45bfdcaa8979f3b191b6f9d
shin5ok <shin5ok@55mp.com>
2016-10-04 09:03:21 +0900

    modify

--- lib/Storage/Select/roundrobin.pm
+++ lib/Storage/Select/roundrobin.pm
@@ -2,13 +2,38 @@ use strict;
 use warnings;
 package Storage::Select::roundrobin 0.01 {
   use Data::Dumper;
+  use File::Basename;
   use Storage::Select;
   use base qw(Storage::Select);
+
+  my $counter = sprintf "/var/tmp/%s.count", basename __FILE__;
+  my $storages_ref;
 
   sub get {
     my ($self, $name) = @_;
     my $config = $self->{config};
-    return Storage->new( $config->{storage}->{$storage_name} );
+
+    $storages_ref //= [ keys %{$config->{storage}} ];
+
+    my $index = _get_index();
+
+    return Storage->new( $config->{storage}->{$storages_ref->[$index]} );
+  }
+
+  sub _get_index {
+    do {
+      open my $fh, ">", $counter if ! -f $counter;
+    };
+    open my $fh, "+<", $counter;
+    flock $fh, 2;
+    seek $fh, 0, 0;
+    my $data = <$fh>;
+    $data //= 0;
+    chomp $data;
+    seek $fh, 0, 0;
+    truncate $fh, 0;
+    print {$fh} $data++;
+    return $data;
   }
 
 }
